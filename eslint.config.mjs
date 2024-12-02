9653088d71497574fa95a67c5ffef02b7114ad8b
Trucker Mudgeon <121829201+truckermudgeon@users.noreply.github.com>
2024-12-01 17:27:56 -0700

    add eslint config that should've been in 95c3b1f

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      '**/*.d.ts',
+      '.idea',
+      'out',
+      '**/build',
+      '**/public',
+      '**/vitest.config.ts',
+      '**/find-route-worker-wrapper.js',
+    ],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+
+        project: [
+          './packages/apis/*/tsconfig.json',
+          './packages/apps/*/tsconfig.json',
+          './packages/clis/*/tsconfig.json',
+          './packages/libs/*/tsconfig.json',
+        ],
+      },
+    },
+
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
