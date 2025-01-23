6d11eee3b2d075d0b71cb8e2db0dbb19366a42f0
truckermudgeon <121829201+truckermudgeon@users.noreply.github.com>
2025-01-22 22:12:19 -0800

    map: update lat/lon calculations (#39)
    Closes #36.
    
    Note: The map tiles on truckermudgeon.github.io won't be updated to use these calculations until the 1.54 game update.

--- packages/libs/map/projections.ts
+++ packages/libs/map/projections.ts
@@ -1,6 +1,10 @@
 import type { Position } from '@truckermudgeon/base/geom';
-import { toRadians } from '@truckermudgeon/base/geom';
 import * as proj4 from 'proj4';
+
+// The Earth radius is canceled out in the coordinate calculation, so the exact
+// value doesn't matter. The chosen radius is the Clarke 1866 Authalic Sphere.
+const earthRadiusMeters = 6_370_997;
+const lengthOfDegree = (earthRadiusMeters * Math.PI) / 180;
 
 // from def/climate.sii
 const atsDefData = {
@@ -10,24 +14,21 @@ const atsDefData = {
   mapOrigin: [39, -96],
   mapFactor: [-0.00017706234, 0.000176689948],
 } as const;
-const atsScale = Math.abs(
-  lengthOfDegreeAt(atsDefData.mapOrigin[0]) * atsDefData.mapFactor[0],
-); // 19.65665620539649
 const ats = [
   '+proj=lcc', // lambert conformal conic
-  '+units=m',
-  '+ellps=sphere',
+  `+R=${earthRadiusMeters}`,
   `+lat_1=${atsDefData.standardParalel1}`,
   `+lat_2=${atsDefData.standardParalel2}`,
   `+lat_0=${atsDefData.mapOrigin[0]}`,
   `+lon_0=${atsDefData.mapOrigin[1]}`,
-  `+k_0=${1 / atsScale}`,
 ].join(' ');
 
 const fromWgs84ToAtsConverter = proj4.default(ats);
 export const fromAtsCoordsToWgs84 = ([x, y]: Position): Position => {
-  // ATS coords are like LCC coords, except in ATS coords Y grows southward (its sign is reversed).
-  const lccCoords: Position = [x, -y];
+  const lccCoords: Position = [
+    x * atsDefData.mapFactor[1] * lengthOfDegree, // ~19.647 ┬ avg ~19.668
+    y * atsDefData.mapFactor[0] * lengthOfDegree, // ~19.688 ┘
+  ];
   return fromWgs84ToAtsConverter.inverse(lccCoords);
 };
 
@@ -40,56 +41,37 @@ const ets2DefData = {
   mapOffset: [16660, 4150],
   mapFactor: [-0.000171570875, 0.0001729241463],
 } as const;
-const ets2Scale = Math.abs(
-  lengthOfDegreeAt(ets2DefData.mapOrigin[0]) * ets2DefData.mapFactor[0],
-); // 19.083661390678152
-const baseEts2 = [
+const ets2 = [
   '+proj=lcc', // lambert conformal conic
-  '+units=m',
-  '+ellps=sphere',
+  `+R=${earthRadiusMeters}`,
   `+lat_1=${ets2DefData.standardParalel1}`,
   `+lat_2=${ets2DefData.standardParalel2}`,
   `+lat_0=${ets2DefData.mapOrigin[0]}`,
   `+lon_0=${ets2DefData.mapOrigin[1]}`,
-];
-const ets2 = [...baseEts2, `+k_0=${1 / ets2Scale}`].join(' ');
-const uk = [...baseEts2, `+k_0=${1 / (ets2Scale * 0.75)}`].join(' ');
+].join(' ');
 const fromWgs84ToEts2Converter = proj4.default(ets2);
-const fromWgs84ToUkConverter = proj4.default(uk);
 export const fromEts2CoordsToWgs84 = ([x, y]: Position): Position => {
-  // N.B.: all the UK detection and offsetting are just guesses.
-  // Couldn't find anything in def files to do this in a more accurate way.
-
-  // treat all coords up-and-to-the-left of Calais as UK coords
-  const calais = [-31100, -5500];
-  const isUk = x < calais[0] && y < calais[1];
-  const converter = isUk ? fromWgs84ToUkConverter : fromWgs84ToEts2Converter;
   // apply mapOffset to coords before projecting.
   x -= ets2DefData.mapOffset[0];
   y -= ets2DefData.mapOffset[1];
-  // UK coords need even more offsetting
+
+  // UK content is authored at a slightly larger scale (~14.37 vs. ~19.15)
+  const ukScaleFactor = 0.75;
+  // HACK: treat all coords up-and-to-the-left of Calais (-31_100, -5500) as UK
+  // coords. Maybe there's a better point to pick than Calais, but I couldn't
+  // find any clues in the def files.
+  const calais = [-31100, -5500];
+  const isUk =
+    x * ukScaleFactor < calais[0] && //
+    y * ukScaleFactor < calais[1];
   if (isUk) {
-    x -= 16_650; // bigger offset => push UK stuff left
-    y -= 2_700; // smaller offset => push UK stuff down
+    x = (x + calais[0] / 2) * ukScaleFactor;
+    y = (y + calais[1] / 2) * ukScaleFactor;
   }
-  // ETS2 coords are like LCC coords, except in ETS2 coords Y grows southward (its sign is reversed).
-  const lccCoords: Position = [x, -y];
-  return converter.inverse(lccCoords);
+
+  const lccCoords: Position = [
+    x * ets2DefData.mapFactor[1] * lengthOfDegree, // ~19.228 ┬ avg ~19.153
+    y * ets2DefData.mapFactor[0] * lengthOfDegree, // ~19.078 ┘
+  ];
+  return fromWgs84ToEts2Converter.inverse(lccCoords);
 };
-
-// from https://gis.stackexchange.com/questions/75528/understanding-terms-in-length-of-degree-formula/
-function lengthOfDegreeAt(latInDegrees: number): number {
-  const m1 = 111132.92;
-  const m2 = -559.82;
-  const m3 = 1.175;
-  const m4 = -0.0023;
-
-  // Calculate the length of a degree of latitude in meters
-  const lat = toRadians(latInDegrees);
-  return (
-    m1 +
-    m2 * Math.cos(2 * lat) +
-    m3 * Math.cos(4 * lat) +
-    m4 * Math.cos(6 * lat)
-  );
-}
