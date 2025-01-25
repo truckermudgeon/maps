85226deaafd3b2a4949186788a3a112e3c3fcca2
Trucker Mudgeon <121829201+truckermudgeon@users.noreply.github.com>
2025-01-25 12:49:05 -0800

    one toCompassPoint

--- packages/apps/navigator/src/controllers/controls.ts
+++ packages/apps/navigator/src/controllers/controls.ts
@@ -1,5 +1,5 @@
-import { assert } from '@truckermudgeon/base/assert';
 import { action, makeAutoObservable } from 'mobx';
+import { toCompassPoint } from '../base/to-compass-point';
 import { CameraMode } from './constants';
 import type {
   AppClient,
@@ -35,32 +35,9 @@ export class ControlsControllerImpl implements ControlsController {
     // TODO keep returned Unsubscribable
     socket.onPositionUpdate.subscribe(undefined, {
       onData: action(gameState => {
-        store.direction = toDirection(gameState.bearing);
+        store.direction = toCompassPoint(gameState.bearing);
         store.limitMph = gameState.speedLimit;
       }),
     });
-  }
-}
-
-// TODO move this to a shared location
-export function toDirection(bearing: number): Direction {
-  const azimuth = bearing >= 0 ? bearing : 360 + bearing;
-  if (337.5 <= azimuth || azimuth < 22.5) {
-    return 'N';
-  } else if (22.5 <= azimuth && azimuth < 67.5) {
-    return 'NE';
-  } else if (67.5 <= azimuth && azimuth < 112.5) {
-    return 'E';
-  } else if (112.5 <= azimuth && azimuth < 157.5) {
-    return 'SE';
-  } else if (157.5 <= azimuth && azimuth < 202.5) {
-    return 'S';
-  } else if (202.5 <= azimuth && azimuth < 247.5) {
-    return 'SW';
-  } else if (247.5 <= azimuth && azimuth < 292.5) {
-    return 'W';
-  } else {
-    assert(292.5 <= azimuth && azimuth < 337.5);
-    return 'NW';
   }
 }
