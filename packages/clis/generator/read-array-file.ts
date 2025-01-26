769605765dd331edac85d817d61ff8cf97600d57
Trucker Mudgeon <121829201+truckermudgeon@users.noreply.github.com>
2025-01-26 10:48:03 -0800

    generator: add file-read timing info

--- packages/clis/generator/read-array-file.ts
+++ packages/clis/generator/read-array-file.ts
@@ -1,12 +1,18 @@
 import fs from 'fs';
+import path from 'path';
+import { logger } from './logger';
 
 export function readArrayFile<T>(
-  path: string,
+  filepath: string,
   filter?: (t: T) => boolean,
 ): T[] {
-  const results: unknown = JSON.parse(fs.readFileSync(path, 'utf-8'));
+  const start = Date.now();
+  const results: unknown = JSON.parse(fs.readFileSync(filepath, 'utf-8'));
   if (!Array.isArray(results)) {
     throw new Error();
   }
-  return filter ? (results as T[]).filter(filter) : (results as T[]);
+  const filtered = filter ? (results as T[]).filter(filter) : (results as T[]);
+  const end = Date.now();
+  logger.debug((end - start) / 1000, 'seconds:', path.basename(filepath));
+  return filtered;
 }
