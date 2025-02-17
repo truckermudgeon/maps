dcb58729a39a72fdc89496c47944e35b8ab450fd
nautofon <129151465+nautofon@users.noreply.github.com>
2025-02-17 21:35:07 +0100

    update linter configuration (#50)

--- packages/apps/prefabs/src/Details.tsx
+++ packages/apps/prefabs/src/Details.tsx
@@ -20,7 +20,6 @@ export const Details = ({ prefab, locations }: DetailsProps) => {
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { token, path, ...prefabRest } = prefab;
   return (
     <Tabs defaultValue={0}>
